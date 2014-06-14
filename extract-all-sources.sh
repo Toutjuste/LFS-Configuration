b580f2fa5c90bdd4527563c4b3955ca13577e48d
Fabien Caylus <toutjuste13@gmail.com>
2014-06-14 10:05:48 +0200

    Fix tzdata extraction

--- extract-all-sources.sh
+++ extract-all-sources.sh
@@ -6,23 +6,32 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cat $SCRIPT_DIR/wget-list | while read line ; do
   file=$(echo ${line##*/})
   if [ -f $file ] ; then
-      case $file in
-          *.tar.bz2)   tar xvjf $file    ;;
-          *.tar.gz)    tar xvzf $file    ;;
-          *.tar.xz)    tar xvJf $file    ;;
-          *.bz2)       bunzip2 $file     ;;
-          *.rar)       rar x $file       ;;
-          *.gz)        gunzip $file      ;;
-          *.tar)       tar xvf $file     ;;
-          *.tbz2)      tar xvjf $file    ;;
-          *.tgz)       tar xvzf $file    ;;
-          *.txz)       tar xvJf $file    ;;
-          *.zip)       unzip $file       ;;
-          *.Z)         uncompress $file  ;;
-          *.7z)        7z x $file        ;;
-          *.patch)     echo "skip patch file '$file'"  ;;
-          *)           echo "don't know how to extract '$file'..." ;;
-      esac
+      if [[ $file == *tzdata* ]]
+      then
+        TZDATADIR=${file%.tar.gz}
+        mkdir -pv $TZDATADIR
+	cd $TZDATADIR
+        tar xvzf ../$file
+        cd ..
+      else
+        case $file in
+            *.tar.bz2)   tar xvjf $file    ;;
+            *.tar.gz)    tar xvzf $file    ;;
+            *.tar.xz)    tar xvJf $file    ;;
+            *.bz2)       bunzip2 $file     ;;
+            *.rar)       rar x $file       ;;
+            *.gz)        gunzip $file      ;;
+            *.tar)       tar xvf $file     ;;
+            *.tbz2)      tar xvjf $file    ;;
+            *.tgz)       tar xvzf $file    ;;
+            *.txz)       tar xvJf $file    ;;
+            *.zip)       unzip $file       ;;
+            *.Z)         uncompress $file  ;;
+            *.7z)        7z x $file        ;;
+            *.patch)     echo "skip patch file '$file'"  ;;
+            *)           echo "don't know how to extract '$file'..." ;;
+        esac
+      fi
   else
       echo "'$file' is not a valid file!"
   fi
--- init-workspace.sh
+++ init-workspace.sh
@@ -71,6 +71,20 @@ if [ -f ~/.colors ]; then
     . ~/.colors
 fi
 PS1="$E_BBlu[\u$E_BCya@$E_BBlu\h] $E_Cya\$ \w $E_BBlu>$E_RCol "
+
+#Colors
+alias ls='ls --color=auto'
+alias dir='dir --color=auto'
+alias vdir='vdir --color=auto'
+
+alias grep='grep --color=auto'
+alias fgrep='fgrep --color=auto'
+alias egrep='egrep --color=auto'
+
+# some more ls aliases
+alias ll='ls -alF'
+alias la='ls -A'
+alias l='ls -CF'
 EOF
 
 #Move files to lfs dir
